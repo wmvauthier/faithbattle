6f16d58376f38e84ee265765618892b675357e85
C1321100 Wesley Matheus Vauthier <c1321100@interno.bb.com.br>
2024-06-28 21:00:51 -0300

    Commit Inicial

--- integrations/game-room.js
+++ integrations/game-room.js
@@ -1,129 +1,36 @@
-// Generate random room name if needed
-if (!location.hash) {
-  location.hash = Math.floor(Math.random() * 0xffffff).toString(16);
-}
-const roomHash = location.hash.substring(1);
+const boxes = document.querySelectorAll(".box");
+const images = document.querySelectorAll(".image, .image2");
 
-// TODO: Replace with your own channel ID
-const drone = new ScaleDrone("yiS12Ts5RdNhebyM");
-// Room name needs to be prefixed with 'observable-'
-const roomName = "observable-" + roomHash;
-console.log(roomName);
-const configuration = {
-  iceServers: [
-    {
-      urls: "stun:stun.l.google.com:19302",
-    },
-  ],
-};
-let room;
-let pc;
-
-function onSuccess() {}
-function onError(error) {
-  console.error(error);
-}
-
-drone.on("open", (error) => {
-  if (error) {
-    return console.error(error);
-  }
-  room = drone.subscribe(roomName);
-  room.on("open", (error) => {
-    if (error) {
-      onError(error);
-    }
+// Loop through each boxes element
+boxes.forEach((box) => {
+  // When a draggable element is dragged over a box element
+  box.addEventListener("dragover", (e) => {
+    e.preventDefault(); // Prevent default behaviour
+    box.classList.add("hovered");
   });
-  // We're connected to the room and received an array of 'members'
-  // connected to the room (including us). Signaling server is ready.
-  room.on("members", (members) => {
-    console.log("MEMBERS", members);
-    // If we are the second user to connect to the room we will be creating the offer
-    const isOfferer = members.length === 2;
-    startWebRTC(isOfferer);
+
+  // When a draggable element leaves a box element
+  box.addEventListener("dragleave", () => {
+    box.classList.remove("hovered");
+  });
+
+  // When a draggable element is dropped on a box element
+  box.addEventListener("drop", (e) => {
+    e.preventDefault();
+    const draggedElementId = e.dataTransfer.getData("text");
+    const draggedElement = document.getElementById(draggedElementId);
+    box.appendChild(draggedElement);
+    box.classList.remove("hovered");
   });
 });
 
-// Send signaling data via Scaledrone
-function sendMessage(message) {
-  drone.publish({
-    room: roomName,
-    message,
+// Loop through each draggable image element
+images.forEach((image) => {
+  // Set unique ID for each draggable element
+  image.id = `draggable-${Math.random().toString(36).substr(2, 9)}`;
+
+  // When a draggable element starts to be dragged
+  image.addEventListener("dragstart", (e) => {
+    e.dataTransfer.setData("text", e.target.id);
   });
-}
-
-function startWebRTC(isOfferer) {
-  pc = new RTCPeerConnection(configuration);
-
-  // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
-  // message to the other peer through the signaling server
-  pc.onicecandidate = (event) => {
-    if (event.candidate) {
-      sendMessage({ candidate: event.candidate });
-    }
-  };
-
-  // If user is offerer let the 'negotiationneeded' event create the offer
-  if (isOfferer) {
-    pc.onnegotiationneeded = () => {
-      pc.createOffer().then(localDescCreated).catch(onError);
-    };
-  }
-
-  // When a remote stream arrives display it in the #remoteVideo element
-  pc.ontrack = (event) => {
-    const stream = event.streams[0];
-    if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
-      remoteVideo.srcObject = stream;
-    }
-  };
-
-  navigator.mediaDevices
-    .getUserMedia({
-      audio: true,
-      video: true,
-    })
-    .then((stream) => {
-      // Display your local video in #localVideo element
-      localVideo.srcObject = stream;
-      // Add your stream to be sent to the conneting peer
-      stream.getTracks().forEach((track) => pc.addTrack(track, stream));
-    }, onError);
-
-  // Listen to signaling data from Scaledrone
-  room.on("data", (message, client) => {
-    // Message was sent by us
-    if (client.id === drone.clientId) {
-      return;
-    }
-
-    if (message.sdp) {
-      // This is called after receiving an offer or answer from another peer
-      pc.setRemoteDescription(
-        new RTCSessionDescription(message.sdp),
-        () => {
-          // When receiving an offer lets answer it
-          if (pc.remoteDescription.type === "offer") {
-            pc.createAnswer().then(localDescCreated).catch(onError);
-          }
-        },
-        onError
-      );
-    } else if (message.candidate) {
-      // Add the new ICE candidate to our connections remote description
-      pc.addIceCandidate(
-        new RTCIceCandidate(message.candidate),
-        onSuccess,
-        onError
-      );
-    }
-  });
-}
-
-function localDescCreated(desc) {
-  pc.setLocalDescription(
-    desc,
-    () => sendMessage({ sdp: pc.localDescription }),
-    onError
-  );
-}
+});
