239c0328a343d5878080ef0a77f2cba42fe57759
C1321100 Wesley Matheus Vauthier <c1321100@interno.bb.com.br>
2024-07-02 17:50:03 -0300

    Commit Inicial

--- integrations/game-room.js
+++ integrations/game-room.js
@@ -1,14 +1,40 @@
 // Seleciona todas as caixas e imagens
 const boxes = document.querySelectorAll(".box");
 const boxesPlayer2 = player2Area.querySelectorAll(".box");
+
+const preparationZonePlayer2 = document.querySelector(
+  "#preparationZonePlayer2"
+);
 const graveyardZonePlayer2 = document.querySelector("#graveyardZonePlayer2");
 const extraZonePlayer2 = document.querySelector("#extraZonePlayer2");
+const deckZonePlayer2 = document.querySelector("#deckZonePlayer2");
 
 const boxesGraveyardZonePlayer2 = graveyardZonePlayer2.querySelectorAll(".box");
 const boxesExtraZonePlayer2 = extraZonePlayer2.querySelectorAll(".box");
+const boxesPreparationZonePlayer2 =
+  preparationZonePlayer2.querySelectorAll(".box");
 
 const boxesBattleArea = battleArea.querySelectorAll(".box");
-const images = player2Area.querySelectorAll(".image");
+let images = document.querySelectorAll(".image");
+
+//CONTADORES DO HUD  -------------------------------------------------------------------------
+
+let player2GamerTag = document.querySelector("#player2GamerTag");
+let player2DeckSize = document.querySelector("#player2DeckSize");
+let player2HandSize = document.querySelector("#player2HandSize");
+let player2GravSize = document.querySelector("#player2GravSize");
+let player2FaithSize = document.querySelector("#player2FaithSize");
+
+let player1FaithCounter = 0;
+let player2FaithCounter = 0;
+
+//VARIÁVEIS INICIAIS -------------------------------------------------------------------------
+
+let deck = [
+  1, 5, 5, 9, 9, 11, 12, 12, 47, 47, 23, 24, 26, 43, 44, 50, 17, 17, 18, 18, 19,
+  19, 29, 29, 30, 30, 42, 42, 48, 48,
+];
+let cardsFromDeck = [];
 
 // Atualiza a lista de caixas da zona conhecida
 const knownZoneBoxes = [];
@@ -19,10 +45,13 @@ for (let i = 1; i <= 10; i++) {
   }
 }
 
-// Preenche os ícones nas caixas
-boxes.forEach((box) => {
-  fillIconBox(box);
-});
+const handZoneBoxes = [];
+for (let i = 1; i <= 9; i++) {
+  const element = document.getElementById(`handZone${i}`);
+  if (element) {
+    handZoneBoxes.push(element);
+  }
+}
 
 // Define as caixas que podem aceitar drag and drop
 const droppableBoxes = [
@@ -30,6 +59,8 @@ const droppableBoxes = [
   ...boxesBattleArea,
   ...boxesGraveyardZonePlayer2,
   ...boxesExtraZonePlayer2,
+  ...handZoneBoxes,
+  ...boxesPreparationZonePlayer2,
 ].filter((box) => !box.classList.contains("knownZoneBox"));
 
 // Adiciona os eventos de drag and drop nas caixas droppable
@@ -45,23 +76,26 @@ droppableBoxes.forEach((box) => {
 
   box.addEventListener("drop", (e) => {
     e.preventDefault();
-    const draggedElementId = e.dataTransfer.getData("text");
-    const draggedElement = document.getElementById(draggedElementId);
+    const draggedElementId = e.dataTransfer.getData("text/plain");
 
-    if (
-      box.children.length === 1 &&
-      box.children[0].tagName.toLowerCase() === "i"
-    ) {
-      box.replaceChild(draggedElement, box.children[0]);
-    } else {
-      box.appendChild(draggedElement);
-    }
+    setTimeout(() => {
+      const draggedElement = document.getElementById(draggedElementId);
 
-    box.classList.remove("hovered");
+      if (draggedElement) {
+        if (
+          box.children.length === 1 &&
+          box.children[0].tagName.toLowerCase() === "i"
+        ) {
+          box.replaceChild(draggedElement, box.children[0]);
+        } else {
+          box.appendChild(draggedElement);
+        }
 
-    boxes.forEach((box) => {
-      fillIconBox(box);
-    });
+        box.classList.remove("hovered");
+
+        renderBattlefield();
+      }
+    }, 50);
   });
 });
 
@@ -73,42 +107,102 @@ knownZoneBoxes.forEach((box) => {
   }
 
   // Adiciona o evento de clique para alternar a imagem
-  box.addEventListener("click", () => toggleImage(box));
+  box.addEventListener("click", () => toggleKnownCard(box));
 });
 
-// Adiciona os eventos de mouseover para mostrar a prévia da imagem
-document.querySelectorAll(".box .image").forEach((imageDiv) => {
-  imageDiv.addEventListener("mouseover", () => {
-    const classes = imageDiv.classList;
-    let imageUrl = "";
+boxes.forEach((box) => {
+  box.style.boxShadow = "1px 1px 1px rgba(249, 250, 251, 1)"; // Branco
+});
 
-    classes.forEach((className) => {
-      if (className.startsWith("image")) {
-        const bgImage = imageDiv.style.backgroundImage;
-        imageUrl = bgImage.slice(5, -2); // Extrai a URL da imagem de fundo
+renderBattlefield();
+
+//DECK ---------------------------------------------------------------------------------
+
+deckZonePlayer2.addEventListener("drop", (e) => {
+  const draggedElementId = e.dataTransfer.getData("text/plain");
+  const draggedElement = document.getElementById(draggedElementId);
+
+  if (draggedElement) {
+    let cardDroppedId = draggedElement.style["background-image"]
+      .split("/")[2]
+      .split(".")[0];
+    deck.push(parseInt(cardDroppedId));
+  }
+
+  deckZonePlayer2.style.backgroundImage = "url('./img/versos/normal.png')";
+
+  if (draggedElement) {
+    draggedElement.remove();
+  }
+
+  renderBattlefield();
+});
+
+//DECK -------------------------------------------------------------------------------
+
+function shuffleDeck() {
+  // Cria uma cópia do array original para não modificá-lo diretamente
+  let shuffledArray = deck.slice();
+
+  // Função de comparação para o método sort()
+  function compareRandom() {
+    return Math.random() - 0.5; // Retorna um número aleatório entre -0.5 e 0.5
+  }
+
+  // Embaralha o array usando o método sort() com a função de comparação
+  shuffledArray.sort(compareRandom);
+  shuffledArray.sort(compareRandom);
+
+  deck = shuffledArray;
+}
+
+function drawCard() {
+  let choosenCardID = deck.pop();
+  let choosenCard = cardsFromDeck.filter((a) => a.number == choosenCardID);
+
+  for (const handZoneBox of handZoneBoxes) {
+    if (
+      !handZoneBox.querySelector("div.image") &&
+      choosenCardID &&
+      choosenCard &&
+      choosenCard[0]
+    ) {
+      const newDiv = document.createElement("div");
+      newDiv.classList.add("image");
+      newDiv.draggable = true;
+      newDiv.style.backgroundImage = "url('" + choosenCard[0].img + "')";
+      handZoneBox.innerHTML = "";
+
+      if (choosenCard[0].type == "Milagre") {
+        newDiv.style.boxShadow = "1px 1px 1px rgba(34, 211, 153, 1)"; // Verde
+      } else if (choosenCard[0].type == "Herói de Fé") {
+        newDiv.style.boxShadow = "1px 1px 1px rgba(59, 130, 246, 1)"; // Azul
+      } else if (choosenCard[0].type == "Pecado") {
+        newDiv.style.boxShadow = "1px 1px 1px rgba(255, 255, 0, 1)";
+      } else if (choosenCard[0].type == "Artefato") {
+        newDiv.style.boxShadow = "1px 1px 1px rgba(251, 191, 36, 1)"; // Amarelo
+      } else {
+        newDiv.style.boxShadow = "1px 1px 1px rgba(249, 250, 251, 1)"; // Branco
       }
-    });
 
-    document.querySelector(".previewImgHover").src = imageUrl;
-  });
-});
+      handZoneBox.appendChild(newDiv);
+      break;
+    }
+  }
 
-// Define os eventos de dragstart para as imagens
-images.forEach((image) => {
-  image.id = `draggable-${Math.random().toString(36).substr(2, 9)}`;
+  renderBattlefield();
+}
 
-  image.addEventListener("dragstart", (e) => {
-    e.dataTransfer.setData("text", e.target.id);
-  });
-});
+//SABEDORIAS -------------------------------------------------------------------------
 
-// Função para adicionar uma nova carta à zona conhecida
 function addKnown() {
   for (const knownZoneBox of knownZoneBoxes) {
-    if (!knownZoneBox.querySelector('div.image')) {
+    if (!knownZoneBox.querySelector("div.image")) {
       const newDiv = document.createElement("div");
       newDiv.classList.add("image");
       newDiv.style.backgroundImage = "url('./img/versos/sabedoria.png')";
+      newDiv.style["background-size"] = "cover";
+      newDiv.style["background-position"] = "center 24%";
       knownZoneBox.innerHTML = "";
 
       knownZoneBox.appendChild(newDiv);
@@ -118,37 +212,35 @@ function addKnown() {
 
   // Atualiza a funcionalidade de drag para as novas imagens
   knownZoneBoxes.forEach((box) => {
-    const imageDiv = box.querySelector('div.image');
+    const imageDiv = box.querySelector("div.image");
     if (imageDiv) {
       imageDiv.removeAttribute("draggable");
     }
   });
 
-  boxes.forEach((box) => {
-    fillIconBox(box);
-  });
+  renderBattlefield();
 }
 
 // Função para remover uma carta da zona conhecida
 function removeKnown() {
   for (let i = knownZoneBoxes.length - 1; i >= 0; i--) {
     const knownZoneBox = knownZoneBoxes[i];
-    const imageDiv = knownZoneBox.querySelector('div.image');
+    const imageDiv = knownZoneBox.querySelector("div.image");
     if (imageDiv) {
       knownZoneBox.removeChild(imageDiv);
       break;
     }
   }
-  boxes.forEach((box) => {
-    fillIconBox(box);
-  });
+  renderBattlefield();
 }
 
 // Função para alternar a imagem de fundo ao clicar na caixa
-function toggleImage(box) {
+function toggleKnownCard(box) {
   const imageDiv = box.querySelector("div.image");
   if (imageDiv) {
     const currentImage = imageDiv.style.backgroundImage;
+    imageDiv.style["background-size"] = "cover";
+    imageDiv.style["background-position"] = "center 24%";
 
     if (currentImage.includes("sabedoria.png")) {
       imageDiv.style.backgroundImage = "url('./img/sabedorias/37.png')";
@@ -156,6 +248,99 @@ function toggleImage(box) {
       imageDiv.style.backgroundImage = "url('./img/versos/sabedoria.png')";
     }
   }
+  renderBattlefield();
+}
+
+//PONTOS DE FÉ ----------------------------------------------------------------------
+
+function addFaith() {
+  player2FaithCounter++;
+  renderBattlefield();
+}
+
+function removeFaith() {
+  if (player2FaithCounter > 0) player2FaithCounter--;
+  renderBattlefield();
+}
+
+//ATUALIZAR TELA --------------------------------------------------------------------
+
+async function renderBattlefield() {
+  async function loadDeck() {
+    let allCards = await getCards();
+    cardsFromDeck = getCardsFromDeck(deck, allCards);
+  }
+
+  await loadDeck();
+
+  if (deck.length > 0) {
+    deckZonePlayer2.style.backgroundImage = "url('./img/versos/normal.png')";
+  } else {
+    deckZonePlayer2.style.backgroundImage = "";
+  }
+  deckZonePlayer2.style["background-size"] = "cover";
+  deckZonePlayer2.style["background-position"] = "center 24%";
+  deckZonePlayer2.style["cursor"] = "pointer";
+
+  let player2HandSizeCnt = 0;
+  let player2GravSizeCnt = 0;
+
+  for (const handZoneBox of handZoneBoxes) {
+    if (handZoneBox.querySelector("div.image")) {
+      player2HandSizeCnt++;
+    }
+  }
+  for (const handZoneBox of boxesGraveyardZonePlayer2) {
+    if (handZoneBox.querySelector("div.image")) {
+      player2GravSizeCnt++;
+    }
+  }
+
+  player2GamerTag.innerHTML =
+    '<i class="fa-solid fa-circle-user"></i>&nbsp;#TENMA007';
+  player2DeckSize.innerHTML =
+    '<i class="fa-solid fa-database"></i>&nbsp;' +
+    formatNumber00(deck.length) +
+    "";
+  player2HandSize.innerHTML =
+    '<i class="fa-solid fa-hand"></i>&nbsp;' +
+    formatNumber00(player2HandSizeCnt);
+  player2GravSize.innerHTML =
+    '<i class="fa-solid fa-skull"></i>&nbsp;' +
+    formatNumber00(player2GravSizeCnt);
+  player2FaithSize.innerHTML =
+    '<i class="fa-solid fa-heart-pulse"></i>&nbsp;' +
+    formatNumber00(player2FaithCounter);
+
+  let images = document.querySelectorAll(".image");
+
+  images.forEach((image) => {
+    image.id = `draggable-${Math.random().toString(36).substr(2, 9)}`;
+
+    image.addEventListener("dragstart", (e) => {
+      e.dataTransfer.setData("text", e.target.id);
+    });
+  });
+
+  document.querySelectorAll(".image").forEach((imageDiv) => {
+    imageDiv.addEventListener("mouseover", () => {
+      const classes = imageDiv.classList;
+      let imageUrl = "";
+
+      classes.forEach((className) => {
+        if (className.startsWith("image")) {
+          const bgImage = imageDiv.style.backgroundImage;
+          imageUrl = bgImage.slice(5, -2); // Extrai a URL da imagem de fundo
+        }
+      });
+
+      document.querySelector(".previewImgHover").src = imageUrl;
+    });
+  });
+
+  boxes.forEach((box) => {
+    fillIconBox(box);
+  });
 }
 
 // Função para preencher a caixa com um ícone se estiver vazia
@@ -187,4 +372,8 @@ function fillIconBox(box) {
       box.appendChild(iconElement);
     }
   }
+}
+
+function formatNumber00(num) {
+  return num.toString().padStart(2, "0");
 }
