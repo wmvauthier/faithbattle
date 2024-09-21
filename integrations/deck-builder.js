5edcb23cd25a9746e14e2ac8b50d9c6f50ca0257
C1321100 Wesley Matheus Vauthier <c1321100@interno.bb.com.br>
2024-09-21 01:18:14 -0300

    Commit Inicial

--- integrations/deck-builder.js
+++ integrations/deck-builder.js
@@ -867,6 +867,7 @@ function updateDeckListDOM(cardsFromDeck) {
       cardElement.addEventListener("click", () =>
         removeCardFromDeckBuilder(card.number)
       );
+      cardElement.style = "padding-right:5px; padding-left: 5px;";
 
       extraDeckListContainer.appendChild(cardElement);
     } else {
@@ -935,7 +936,7 @@ function updateMiniCards(allCards, cardsList, id) {
     if (details) {
       const cardElement = document.createElement("div");
       cardElement.className =
-        "col-lg-1 col-md-1 col-sm-2 col-2 card__related__sidebar__view__item set-bg";
+        "col-lg-2 col-md-2 col-sm-2 col-2 card__related__sidebar__view__item set-bg";
       cardElement.style.cursor = "pointer";
       cardElement.innerHTML = `
         <img class="card__details set-card-bg" src="${details.img}" alt="${details.name}" />
