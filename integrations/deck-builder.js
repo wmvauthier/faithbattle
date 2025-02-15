fe2fc30ca23ba5af7fc01722f397c4528e8ff250
C1321100 Wesley Matheus Vauthier <c1321100@interno.bb.com.br>
2025-02-14 23:39:53 -0300

    Commit Inicial

--- integrations/deck-builder.js
+++ integrations/deck-builder.js
@@ -418,7 +418,7 @@ function exportDeck() {
   // Monta o texto final conforme o formato do Marvel SNAP
   const finalOutput = `${textLines.join(
     "\n"
-  )}\n#\n${base64String}\n#\n# Para usar este deck, copie-o para a área de transferência e cole a partir do menu de edição de decks no FAITH BATTLE .`;
+  )}\n#\n${base64String}\n#\n# Para usar este deck, copie-o para a área de transferência e cole a partir do DeckBuilder no site do FAITH BATTLE .`;
 
   navigator.clipboard
     .writeText(finalOutput)
